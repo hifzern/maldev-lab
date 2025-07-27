669d2753a63ba15c190a236a5f2ea1b9e18741f4
adh1ka <hafizhadhi27@gmail.com>
2025-07-28 02:27:23 +0700

    continue develop rc4 encrypt feature

--- rc4encrypt1/rc4encrypt1.cpp
+++ rc4encrypt1/rc4encrypt1.cpp
@@ -39,7 +39,41 @@ void rc4Init(Rc4Context* context, const unsigned char* key, size_t length)
 }
 
 void rc4Cipher(Rc4Context* context, const unsigned char* input, unsigned char* output, size_t length) {
-    unsigned
+    unsigned char temp;
+
+    //restore context
+    unsigned int i = context->i;
+    unsigned int j = context->j;
+    unsigned char* s = context->s;
+
+    //encrypt loop
+    while (length > 0) {
+        //adjust indices
+        i = (i + 1) % 256;
+        j = (j + s[i]) % 256;
+
+        //swap
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+
+        //check input
+        if (input != NU:: && output != NULL)
+        {
+            //xor input with rc4 stream
+            *output = *input ^ s[(s[i] + s[j]) % 256;
+
+            //increment pointers
+            input++;
+            output++;
+        }
+
+        //remaining bytes
+        length--;
+    }
+    //save context
+    context->i = i;
+    context->j = j;
 }
 
 
