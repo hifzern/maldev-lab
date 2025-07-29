88f8a2c524ed0cc124ec3c0d57849d74d9964f12
adh1ka <hafizhadhi27@gmail.com>
2025-07-29 14:38:36 +0700

    mager bgt kontol

--- ipv4fuscation/ipv4fuscation/ipv4fuscation.cpp
+++ ipv4fuscation/ipv4fuscation/ipv4fuscation.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
 #include <Windows.h>
 
-// 
+// function takes in 4 raw bytes and return in ipv4
+char* GenerateIpv4(int a, int b, int c, int d) {
+    unsigned char Output [32];
+
+    //creating the ipv4 address and saving it into the output variable
+    sprintf(Output, "%d.%d.%d.%d", a, b, c, d);
+
+    //print output
+    printf("[i] Output : %s \n", Output);
+
+    return (char*)Output;
+}
+
+//generate the ipv4 output representation of the shellcode
+//function requires a pointer or base address to the shellcode buffer & the size of the shellcode buffer
+BOOL GenerateIpv4Output(unsigned char* pShellcode, SIZE_T ShellcodeSize) {
+    //if the shellcode buffer is null or the size is not a multiple of 4
+    if (pShellcode == NULL || ShellcodeSize == NULL || ShellcodeSize % 4 != 0) {
+        return FALSE;
+    }
+    printf("char* Ipv4Array[%d] = { \n\t", (int)(ShellcodeSize / 4));
+
+    // we will read one shellcode byte at a time, when the total is 4, begin generating the ipv4 address
+    //the variable c is used to store the number of bytes read. by default starts at 4.
+
+    int c = 4, counter = 0;
+    char* IP = NULL;
+
+    for (int i = 0; i < ShellcodeSize; i++) {
+        //track the number of bytes read and when they reach 4 we enter this if statement to begin generating the ipv4 address
+        if (c == 4) {
+            counter++;
+
+            //generate ipv4 address from 5 bytes which begin at i until [i + 3]
+        }
+    }
+}
 
 int main()
 {
