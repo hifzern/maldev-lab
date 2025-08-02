04116124d4839e2bac52778980b86c4ef75a1513
adh1ka <hafizhadhi27@gmail.com>
2025-08-03 02:57:04 +0700

    buat func GetRemoteProcessHandle

--- dll-injection/dll-injection/dll-injection.cpp
+++ dll-injection/dll-injection/dll-injection.cpp
@@ -2,45 +2,56 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 
-//takes snapshot of the currently running processes
-hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+BOOL GetRemoteProcessHandle(IN LPWSTR szProcessName, OUT DWORD* dwProcessId, OUT HANDLE* hProcess) {
+	//according to the documentation
+	//before calling the Process32First function, set this member to sizeof(PROCESSENTRY32).
+	//if dwSize is not initialized
+	PROCESSENTRY32 Proc = {
+		.dwSize = sizeof(PROCESSENTRY32)
+	};
 
-typedef struct tagPROCESSENTRY32 {
-	DWORD		dwSize;
-	DWORD		cntUsage;
-	DWORD		th32ProcessID;			//PROCESS ID
-	ULONG_PTR	th32DefaultHeapID;
-	DWORD		th32ModuleID;
-	DWORD		cntThreads;
-	DWORD		th32ParentProcessID;		//PROCESS ID OF THE PARENT PROCESS
-	LONG		pcPriClassBase;
-	DWORD		dwFlags;
-	CHAR		szExeFile[MAX_PATH];		//THE NAME OF THE EXECUTABLE FILE FOR THE PROCESS
-} PROCESENTRY32;
+	HANDLE hSnapshot = NULL;
 
-//retrieves information about the first process encountered in the snapshot
-if (!Process32First(hSnapshot, &Proc)) {
-	std::cout << "[!] Process32First Failed with error : %d \n" << GetLastError << endl;
-	goto _EndOfFunction;
-}
-
-do {
-	//use the dot operator to extract the process name from the populated struct
-	//if the process name matches the process we're looking for
-	if (wcscmp(Proc.szExFile, szProcessName) == 0) {
-		// use the dot operator to extract the process id from the populated struct
-		// save the pid
-		*dwProcessId = Proc.th43ProcessID;
-
-		//open the handle to process
-		*hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, Proc.th32ProcessID);
-		if (*hProcess == NULL) {
-			std::cout << "[!] OpenProcess Failed With error : %d \n" << GetLastError << endl;
-		}
-
-		break; //exit the loop
+	// Takes a snapshot of the currently running processes
+	hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	if (hSnapshot == INVALID_HANDLE_VALUE) {
+		printf("[!] CreateToolhelp32Snapshot Failed with error : %d \n", GetLastError());
+		goto _EndOfFunction;
 	}
-//retrieves information about the next process recorded the snapshot
-// while a process still remains in the snapshot, continue looping
 
-} while (Process32Next(hSnapShot, &Proc));
+	//retrieves information about the first process encountered in the snapshot
+	if (!Process32First(hSnapshot, &Proc)) {
+		printf("[!] Process32First Failed with error : %d \n", GetLastError());
+		goto _EndOfFunction;
+	}
+
+	do {
+		// use the dot operator to extract th eprocess name from the populated struct
+		// if the process name matches the process we're looking for
+		if (wcscmp(Proc.szExeFile, szProcessName) == 0) {
+			// use the dot operator to extract the process ID from the popiulated struct
+			//save the PID
+			*dwProcessId = Proc.th32ProcessID;
+
+			//open handler to the proceess
+			*hprocess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, Proc.th32ProcessID);
+			if (*hProcess = NULL) {
+				printf("[!] OpenProcess Failed with error : %d \n", GetLastError());
+			}
+			break; //exit the loop
+		}
+		//retrieves information about the next process recorded the snapshot
+		//while process still remains in the snapsho, continuet looping
+	} while (Process32Next(hSnapShot, &Proc));
+
+	//cleanup
+_EndOfFunction: 
+	if (hSnapshot != NULL)
+		CloseHandle(hSnapshot);
+	if (*dwProcessId == NULL || *hProcess == NULL)
+		return FALSE;
+	return TRUE;
+
+	
+
+}
