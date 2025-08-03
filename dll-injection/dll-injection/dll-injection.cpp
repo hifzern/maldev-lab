5979ef46d07b5910d51115750e2cdace6dbe586c
adh1ka <hafizhadhi27@gmail.com>
2025-08-03 15:05:59 +0700

    rewrite

--- dll-injection/dll-injection/dll-injection.cpp
+++ dll-injection/dll-injection/dll-injection.cpp
@@ -1,59 +1,60 @@
 #include <iostream>
 #include <Windows.h>
 #include <TlHelp32.h>
+#include <tchar.h>
 
 BOOL GetRemoteProcessHandle(IN LPWSTR szProcessName, OUT DWORD* dwProcessId, OUT HANDLE* hProcess) {
 	//according to the documentation
 	//before calling the Process32First function, set this member to sizeof(PROCESSENTRY32).
 	//if dwSize is not initialized
-	PROCESSENTRY32 Proc = {
-		.dwSize = sizeof(PROCESSENTRY32)
-	};
-
-	HANDLE hSnapshot = NULL;
+	PROCESSENTRY32 procEntry = { 0 };
+	procEntry.dwSize = sizeof(PROCESSENTRY32);
 
 	// Takes a snapshot of the currently running processes
-	hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
 	if (hSnapshot == INVALID_HANDLE_VALUE) {
-		printf("[!] CreateToolhelp32Snapshot Failed with error : %d \n", GetLastError());
-		goto _EndOfFunction;
+		std::wcerr << L"[!] CreateToolhelp32Snapshot Failed with error : \n" << GetLastError() << std::endl;
+		return FALSE;
 	}
+
+	BOOL found = FALSE;
 
 	//retrieves information about the first process encountered in the snapshot
-	if (!Process32First(hSnapshot, &Proc)) {
-		printf("[!] Process32First Failed with error : %d \n", GetLastError());
-		goto _EndOfFunction;
-	}
+	if (!Process32First(hSnapshot, &procEntry)) {
+		do {
+			// use the dot operator to extract th eprocess name from the populated struct
+			// if the process name matches the process we're looking for
+			if (_wcsicmp(procEntry.szExeFile, szProcessName) == 0) {
+				// use the dot operator to extract the process ID from the popiulated struct
+				//save the PID
+				*dwProcessId = procEntry.th32ProcessID;
 
-	do {
-		// use the dot operator to extract th eprocess name from the populated struct
-		// if the process name matches the process we're looking for
-		if (wcscmp(Proc.szExeFile, szProcessName) == 0) {
-			// use the dot operator to extract the process ID from the popiulated struct
-			//save the PID
-			*dwProcessId = Proc.th32ProcessID;
+				//open handler to the proceess
+				*hProcess = OpenProcess(
+					PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, 
+					FALSE, 
+					procEntry.th32ProcessID
+				);
 
-			//open handler to the proceess
-			*hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, Proc.th32ProcessID);
-			if (*hProcess = NULL) {
-				printf("[!] OpenProcess Failed with error : %d \n", GetLastError());
+				if (*hProcess == NULL) {
+					std::wcerr << "[!] OpenProcess Failed with error : \n" << GetLastError() << std::endl;
+					CloseHandle(hSnapshot);
+					return FALSE;
+				}
+				found = TRUE;
+				break; //exit the loop
 			}
-			break; //exit the loop
-		}
-		//retrieves information about the next process recorded the snapshot
-		//while process still remains in the snapsho, continuet looping
-	} while (Process32Next(hSnapshot, &Proc));
-
-	//cleanup
-_EndOfFunction: 
-	if (hSnapshot != NULL)
-		CloseHandle(hSnapshot);
-	if (*dwProcessId == NULL || *hProcess == NULL)
-		return FALSE;
-	return TRUE;
+			//retrieves information about the next process recorded the snapshot
+			//while process still remains in the snapsho, continuet looping
+		} while (Process32Next(hSnapshot, &procEntry));
+	}
+	CloseHandle(hSnapshot);
+	return found;
 }
 
 BOOL InjectDllToRemoteProcess(IN HANDLE hProcess, IN LPWSTR DllName) {
+	SIZE_T dllPathLen = (wcslen(dllPath))
+
 	BOOL bSTATE = TRUE;
 	LPVOID pLoadLibraryW = NULL;
 	LPVOID pAddress = NULL;
@@ -61,21 +62,21 @@ BOOL InjectDllToRemoteProcess(IN HANDLE hProcess, IN LPWSTR DllName) {
 	// FETCHING THE SIZE OF DLLNAME IN BYTES
 	DWORD dwSizeToWrite = lstrlenW(DllName) * sizeof(WCHAR);
 
-	SIZE_T lpNumberOfBytesWritten = NULL;
+	SIZE_T lpNumberOfBytesWritten = 0;
 	HANDLE hThread = NULL;
 
 	pLoadLibraryW = GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryW");
 	if (pLoadLibraryW == NULL) {
 		printf("[!] GetProcAddress Failed with error : %d \n", GetLastError());
 		bSTATE = FALSE;
-		goto _EndOfFunction;
+		return FALSE;
 	}
 
 	pAddress = VirtualAllocEx(hProcess, NULL, dwSizeToWrite, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 	if (pAddress == NULL) {
 		printf("[!] VirtualAllocEx Failed with error : %d \n", GetLastError());
 		bSTATE = FALSE;
-		goto _EndOfFunction;
+		return FALSE;
 	}
 	printf("[i] pAddress Allocated at : 0x%p Of Size : %d \n", pAddress, dwSizeToWrite);
 	printf("[#] Press <enter> To Run ...");
@@ -84,7 +85,7 @@ BOOL InjectDllToRemoteProcess(IN HANDLE hProcess, IN LPWSTR DllName) {
 	if (!WriteProcessMemory(hProcess, pAddress, DllName, dwSizeToWrite, &lpNumberOfBytesWritten) || lpNumberOfBytesWritten != dwSizeToWrite) {
 		printf("[!] WriteProcessMemory Failed with error : %d \n", GetLastError());
 		bSTATE = FALSE;
-		goto _EndOfFunction;
+		return FALSE;
 	}
 
 
@@ -97,7 +98,7 @@ BOOL InjectDllToRemoteProcess(IN HANDLE hProcess, IN LPWSTR DllName) {
 	if (hThread == NULL) {
 		printf("[!] CreateRemoteThread Failed with error : %d \n", GetLastError());
 		bSTATE = FALSE;
-		goto _EndOfFunction;
+		return FALSE;
 	}
 	printf("[+] DONE \n");
 
@@ -105,4 +106,21 @@ _EndOfFunction:
 	if (hThread != NULL)
 		CloseHandle(hThread);
 	return bSTATE;
+}
+
+int wmain(int argc, wchar_t* argv[]) {
+	if (argc != 3) {
+		std::wcout << L"Usage: Injector.exe <ProcessName.exe> <PathToDDll.dll>" << std::endl;
+		return 1;
+	}
+
+	DWORD pid = 0;
+	HANDLE hProcess = NULL;
+
+	if (!GetRemoteProcessHandle(argv[1], &pid, &hProcess)) {
+		std::wcerr << L"[!] Failed to get process handle" << std::endl;
+		return 1;
+	}
+
+	std::wcout << L"[+] Found process ID : " << pid << std::endl;
 }
