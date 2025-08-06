4e67af447241075d9e72e839764af0e7663950f6
adh1ka <hafizhadhi27@gmail.com>
2025-08-06 16:59:53 +0700

    HUAH MUALES

--- enum-processes/enum-processes/enum-processes.cpp
+++ enum-processes/enum-processes/enum-processes.cpp
@@ -1,20 +1,23 @@
-// enum-processes.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <windows.h>
+#include <TlHelp32.h>
+#include <Psapi.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+BOOL GetRemoteProcessHandle(LPCWSTR szProcName, DWORD* pdwPid, HANDLE* phProcess) {
+	DWORD adwProcesses[1024 * 2], dwReturnLen1 = NULL, dwReturnLen2 = NULL, dwNmbrOfPids = NULL;
+	
+	//get array of pid
+	if (!EnumProcesses(adwProcesses, sizeof(adwProcesses), &dwReturnLen1)) {
+		std::wcerr << L"[!] EnumProcesses failed with error : " << GetLastError() << std::endl;
+		return FALSE;
+	}
+
+	//calculate the number of element in the array
+	dwNmbrOfPids = dwReturnLen1 / sizeof(DWORD);
+
+	std::wcout << L"" << std::endl;
+	
+
+
+	return TRUE;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
