5f4626cec5ba30376de4af69d8d6e7bc5e2b89a3
adh1ka <hafizhadhi27@gmail.com>
2025-08-08 07:11:39 +0700

    create func GetRemoteProcessHandle

--- enum-processes/enum-processes/enum-processes.cpp
+++ enum-processes/enum-processes/enum-processes.cpp
@@ -5,7 +5,10 @@
 
 BOOL GetRemoteProcessHandle(LPCWSTR szProcName, DWORD* pdwPid, HANDLE* phProcess) {
 	DWORD adwProcesses[1024 * 2], dwReturnLen1 = NULL, dwReturnLen2 = NULL, dwNmbrOfPids = NULL;
-	
+	HANDLE hProcess = NULL;
+	HMODULE hModule = NULL;
+	WCHAR szProc [MAX_PATH];
+
 	//get array of pid
 	if (!EnumProcesses(adwProcesses, sizeof(adwProcesses), &dwReturnLen1)) {
 		std::wcerr << L"[!] EnumProcesses failed with error : " << GetLastError() << std::endl;
@@ -15,8 +18,52 @@ BOOL GetRemoteProcessHandle(LPCWSTR szProcName, DWORD* pdwPid, HANDLE* phProcess
 	//calculate the number of element in the array
 	dwNmbrOfPids = dwReturnLen1 / sizeof(DWORD);
 
-	std::wcout << L"" << std::endl;
+	std::wcout << L"[i] Number of Processes detected : " << dwNmbrOfPids << std::endl;
+
+	for (int i = 0; i < dwNmbrOfPids; i++) {
+		//if process !null
+		if (!adwProcesses[i]) {
+
+
+			//open process handle
+			
+			if (!(hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, adwProcesses[i]))) {
+				//if handle is valid
+				//get andle of a module in the process 'hProcess'
+				//the module handle is needed for 'GetModuleBaseName'
+				if (!EnumProcessModules(hProcess, &hModule, sizeof(HMODULE), &dwReturnLen2)) {
+					std::wcerr << L"[!] EnumProcessModules Failed [ At Pid : " <<adwProcesses[i] << L" ] with error : " << GetLastError() << std::endl;
+				}
+				else {
+					//if enumprocessmodules succeeded
+					//get the name of hProcess and save it in the szProc variable
+					if (!GetModuleBaseName(hProcess, hModule, szProc, sizeof(szProc) / sizeof(WCHAR))) {
+						std::wcerr << L"[!] GetModuleBaseName Failed [ At Pid : " << adwProcesses[i] << L" ] with error : " << GetLastError() << std::endl;
+					}
+					else {
+						//perform the comparison logic
+						if (!wcscmp(szProcName, szProc)) {
+							std::wcout << L"[+] FOUND " << szProc << L" - Of Pid : " << adwProcesses[i] << std::endl;
+
+							//return by reference
+							*pdwPid = adwProcesses[i];
+							*phProcess = hProcess;
+							break;
+						}
+					}
+				}
+				CloseHandle(hProcess);
+			}
+		}
+	}
 	
+	//check if pdwPid or phProcess are NULL
+	if (!*pdwPid || !*phProcess) {
+		return FALSE;
+	}
+	else {
+		return TRUE;
+	}
 
 
 	return TRUE;
