c7f167e428dbbe3a0a7e45c34d38554e59c9e8e7
adh1ka <hafizhadhi27@gmail.com>
2025-08-11 17:08:47 +0700

    init

--- procenum-NtQuerySystemInformation/procenum-NtQuerySystemInformation/procenum-NtQuerySystemInformation.cpp
+++ procenum-NtQuerySystemInformation/procenum-NtQuerySystemInformation/procenum-NtQuerySystemInformation.cpp
@@ -3,5 +3,13 @@
 #include <winternl.h>
 
 BOOL GetRemoteProcessHandle(LPCWSTR szProcName, DWORD* pdwPid, HANDLE* phProcess) {
-	fnNtQuerySystemInformation = (fnNtQuerySystemInformation)GetProcAddress(GetModuleHandle(L"NTDLL.DLL"))
+	fnNtQuerySystemInformation pNtQuerySystemInformation = NULL;
+	pNtQuerySystemInformation = (fnNtQuerySystemInformation)GetProcAddress(GetModuleHandle(L"NTDLL.DLL"),
+		"NtQuerySystemInformation");
+	if (!pNtQuerySystemInformation) {
+		std::wcerr << L"[!] GetProcAddress Failed With error : " << GetLastError() << std::endl;
+		return FALSE;
+	}
+
+	
 }
