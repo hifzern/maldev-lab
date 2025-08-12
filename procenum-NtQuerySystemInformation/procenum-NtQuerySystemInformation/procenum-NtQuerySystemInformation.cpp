e47043f7e0465a1b982f70ecac1cc210bdf6a86b
adh1ka <hafizhadhi27@gmail.com>
2025-08-12 16:18:39 +0700

    ngelanjut

--- procenum-NtQuerySystemInformation/procenum-NtQuerySystemInformation/procenum-NtQuerySystemInformation.cpp
+++ procenum-NtQuerySystemInformation/procenum-NtQuerySystemInformation/procenum-NtQuerySystemInformation.cpp
@@ -27,54 +27,43 @@ BOOL GetRemoteProcessHandle(LPCWSTR szProcName, DWORD* pdwPid, HANDLE* phProcess
 	
 	PBYTE buffer = nullptr;
 
-	pNtQuerySystemInformation = (fnNtQuerySystemInformation)GetProcAddress(GetModuleHandle(L"NTDLL.DLL"),
-		"NtQuerySystemInformation");
-	if (!pNtQuerySystemInformation) {
-		std::wcerr << L"[!] GetProcAddress Failed With error : " << GetLastError() << std::endl;
-		return FALSE;
-	}
+	for (;;) {
+		HeapFree(GetProcessHeap(), 0, buffer);
+		buffer = (PBYTE)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, bufSize);
+		if (!buffer) return FALSE;
 
-	pNtQuerySystemInformation(SystemProcessInformation, NULL, NULL, &uReturnLen1);
-	PSYSTEM_PROCESS_INFORMATION SystemProcInfo = (PSYSTEM_PROCESS_INFORMATION)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, (SIZE_T)uReturnLen1);
-	if (!SystemProcInfo) {
-		std::wcerr << L"[!] HeapAlloc Failed with error : " << GetLastError() << std::endl;
-		return FALSE;
-	}
-
-	//since we'll modify SystemProcInfo, we'll save its initial value before the while loop free it later
-	PVOID pValueToFree = SystemProcInfo;
-	NTSTATUS STATUS = pNtQuerySystemInformation(SystemProcessInformation, SystemProcInfo, uReturnLen1, &uReturnLen2);
-	if (STATUS) {
-		std::wcerr << L"[!] NtQuerySystemInformation Failed With error : " << GetLastError() << std::endl;
-		return FALSE;
-	}
-	while (TRUE) {
-		//check the process name file, comparing the enumerated process name to the intended target process
-		if (!SystemProcInfo->ImageName.Length && wcscmp(SystemProcInfo->ImageName.Buffer, szProcName)) {
-			//opening a handle to the target process, saving it, and then breaking
-			*pdwPid = (DWORD)SystemProcInfo->UniqueProcessId;
-			*phProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, (DWORD)SystemProcInfo->UniqueProcessId);
-			break;
+		status = pNtQuerySystemInformation(SystemProcessInformation, buffer, bufSize, &bufSize);
+		if (status == STATUS_INFO_LENGTH_MISMATCH) continue;
+		if (!NT_SUCCESS(status)) {
+			HeapFree(GetProcessHeap(), 0, buffer); 
+			return FALSE;
 		}
 
-		//if nextentryoffset is 0, we reached the end of the array
-		if (!SystemProcInfo->NextEntryOffset) {
-			break;
-		}
-		//move to the next element in the array
-		SystemProcInfo = (PSYSTEM_PROCESS_INFORMATION)((ULONG_PTR)SystemProcInfo + SystemProcInfo->NextEntryOffset);
+		break;
 	}
 
+	PSYSTEM_PROCESS_INFORMATION spi = (PSYSTEM_PROCESS_INFORMATION)buffer;
+	for (;;) {
+		if (spi->ImageName.Buffer) {
+			if (_wcsicmp(spi->ImageName.Buffer, szProcName) == 0) {
+				DWORD pid = HandleToULong(spi->UniqueProcessId);
+				DWORD desired = PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_VM_READ | SYNCHRONIZE;
+				HANDLE h = OpenProcess(desired, FALSE, pid);
+
+				*pdwPid = pid;
+				*phProcess = h;
+
+				HeapFree(GetProcessHeap(), 0, buffer);
+				return h != nullptr;
+			}
+		}
+		if (spi->NextEntryOffset == 0) break;
+		spi = (PSYSTEM_PROCESS_INFORMATION)((PBYTE)spi + spi->NextEntryOffset);
+	}
 	//free using the initial address
-	HeapFree(GetProcessHeap(), 0, pValueToFree);
-
-	//check if we successfully got the target process handle
-	if (!*pdwPid || !*phProcess) {
-		return FALSE;
-	}
-	else {
-		return TRUE;
-	}
+	HeapFree(GetProcessHeap(), 0, buffer);
+	SetLastError(ERROR_NOT_FOUND);
+	return FALSE;
 }
 
 int wmain(int argc, wchar_t* argv[]) {
